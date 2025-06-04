aee106e1991b6f9e8a7c4d3af3b8e5e0e8c573e6
HSSJW <okqkfkfk@naver.com>
2025-06-04 16:44:16 +0900

    Feat : 과제2

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/ProductController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/ProductController.java
@@ -3,8 +3,10 @@ package kr.ac.hansung.cse.hellospringdatajpa.controller;
 import kr.ac.hansung.cse.hellospringdatajpa.entity.Product;
 import kr.ac.hansung.cse.hellospringdatajpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
